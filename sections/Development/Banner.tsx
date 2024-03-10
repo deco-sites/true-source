362ba4cf9877c8b5d9acb3f96bb1a17dd294d394
Pedro França <jppfeitosa@gmail.com>
2024-03-10 09:30:42 -0300

    feat: add CA Banner

--- sections/Development/Banner.tsx
+++ sections/Development/Banner.tsx
@@ -1,23 +1,26 @@
 import type { ImageWidget } from "apps/admin/widgets.ts";
-import Icon from "$store/components/ui/Icon.tsx";
 import Image from "apps/website/components/Image.tsx";
 
 export interface Props {
-    image: ImageWidget;
+  image: ImageWidget;
 }
 
 export default function Banner({ image }: Props) {
-    return (
-        <div class="flex w-full">
-            <Image
-                width={640}
-                class="w-full h-[240px] object-fit z-10 rounded-b-3xl"
-                sizes="(max-width: 640px) 100vw, 30vw"
-                src={image}
-                alt={image}
-                decoding="async"
-                loading="lazy"
-            />
-        </div>
-    );
+  return (
+    <div class="flex flex-col relative w-full h-[240px] justify-center items-center">
+      <Image
+        width={640}
+        class="w-full h-[240px] object-fit z-10 rounded-b-3xl"
+        sizes="(max-width: 640px) 100vw, 30vw"
+        src={image}
+        alt={image}
+        decoding="async"
+        loading="lazy"
+      />
+
+      <p class="absolute text-4xl text-ice font-bold font-lemon-milk z-20">
+        CENTRAL DE ATENDIMENTO
+      </p>
+    </div>
+  );
 }
