fff0d248e7789c7ff1b9fedb42037985b409abe0
betovawn <sergio.junior@wavecommerce.com.br>
2024-03-13 15:36:28 -0300

    feat: Implementação das dots e regulagem da altura dos cards em Benefits

--- components/product/Description/Benefits.tsx
+++ components/product/Description/Benefits.tsx
@@ -67,9 +67,9 @@ export default function Benefits({
             } = benefit;
 
             return (
-              <div class="swiper-slide !w-[238px] sm:!w-[350px]  first:ml-4 last:mr-4">
+              <div class="swiper-slide !w-[238px] sm:!w-[350px] !h-auto first:pl-4 last:pr-4">
                 <div class="flex flex-none items-center gap-6 bg-ice px-4 py-3 rounded-lg text-sm lg:text-base h-full">
-                  <div class="w-[48px] sm:w-[80px] flex-none">
+                  <div class="w-[48px] sm:w-[80px] flex-none">  
                     <img
                       class="w-[48px] sm:w-[80px] h-auto"
                       width={80}
