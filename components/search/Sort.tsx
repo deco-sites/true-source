052cff536c9d497b3912bb54dcab8338d4fe886b
lui-dias <github.anyplace863@passinbox.com>
2024-03-18 11:10:27 -0300

    fix: Wrong check size

--- components/search/Sort.tsx
+++ components/search/Sort.tsx
@@ -73,7 +73,7 @@ function Sort({ sortOptions, isMobile }: Props) {
               <label
                 for={id}
                 class={clx(
-                  "size-4 border-2 border-dark rounded-full flex justify-center items-center",
+                  "size-[18px] border-2 border-dark rounded-full flex justify-center items-center",
                   selected && "bg-dark",
                 )}
               >
@@ -90,52 +90,50 @@ function Sort({ sortOptions, isMobile }: Props) {
   }
 
   return (
-    <>
-      <sortCollapsable.Collapsable class="relative z-20 w-[200px]">
-        <sortCollapsable.Trigger class="px-3 py-2 rounded-full bg-ice flex justify-center items-center gap-2 text-sm font-bold text-dark relative z-10">
-          Ordenar por
-          <Icon id="ChevronDown" width={16} height={16} />
-        </sortCollapsable.Trigger>
+    <sortCollapsable.Collapsable class="relative z-20 w-[200px]">
+      <sortCollapsable.Trigger class="px-3 py-2 rounded-full bg-ice flex justify-center items-center gap-2 text-sm font-bold text-dark relative z-10">
+        Ordenar por
+        <Icon id="ChevronDown" width={16} height={16} />
+      </sortCollapsable.Trigger>
 
-        <sortCollapsable.ContentWrapper class="absolute left-0 top-[calc(100%-20px)] bg-ice rounded-bl-xl rounded-br-xl w-full">
-          <sortCollapsable.Content>
-            <div class="pt-4">
-              {options.map(({ value, label }) => {
-                const selected = value === sort;
-                const id = useId();
+      <sortCollapsable.ContentWrapper class="absolute left-0 top-[calc(100%-20px)] bg-ice rounded-bl-xl rounded-br-xl w-full">
+        <sortCollapsable.Content>
+          <div class="pt-4">
+            {options.map(({ value, label }) => {
+              const selected = value === sort;
+              const id = useId();
 
-                return (
-                  <button
-                    type="button"
-                    onClick={() => applySort(value)}
-                    class="flex items-center gap-4 text-sm text-dark p-3 w-full"
+              return (
+                <button
+                  type="button"
+                  onClick={() => applySort(value)}
+                  class="flex items-center gap-4 text-sm text-dark p-3 w-full"
+                >
+                  <input
+                    type="checkbox"
+                    id={id}
+                    checked={selected}
+                    class="peer hidden"
+                  />
+                  <label
+                    for={id}
+                    class={clx(
+                      "size-[18px] border-2 border-dark rounded-full flex justify-center items-center",
+                      selected && "bg-dark",
+                    )}
                   >
-                    <input
-                      type="checkbox"
-                      id={id}
-                      checked={selected}
-                      class="peer hidden"
-                    />
-                    <label
-                      for={id}
-                      class={clx(
-                        "size-4 border-2 border-dark rounded-full flex justify-center items-center",
-                        selected && "bg-dark",
-                      )}
-                    >
-                      {selected && (
-                        <span class="size-2 absolute bg-white rounded-full" />
-                      )}
-                    </label>
-                    <span class="text-sm">{label}</span>
-                  </button>
-                );
-              })}
-            </div>
-          </sortCollapsable.Content>
-        </sortCollapsable.ContentWrapper>
-      </sortCollapsable.Collapsable>
-    </>
+                    {selected && (
+                      <span class="size-2 absolute bg-white rounded-full" />
+                    )}
+                  </label>
+                  <span class="text-sm">{label}</span>
+                </button>
+              );
+            })}
+          </div>
+        </sortCollapsable.Content>
+      </sortCollapsable.ContentWrapper>
+    </sortCollapsable.Collapsable>
   );
 }
 
