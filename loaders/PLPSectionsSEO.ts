7d4e13d2ed302e36d6d30beae2e5e853e00ba4e3
lui-dias <github.anyplace863@passinbox.com>
2024-03-21 12:32:42 -0300

    chore: Better admin props names

--- loaders/PLPSectionsSEO.ts
+++ loaders/PLPSectionsSEO.ts
@@ -4,15 +4,28 @@ import { Section } from "deco/blocks/section.ts";
  * @titleBy matcher
  */
 interface O {
+  /**
+   * @title Pathname da URL
+   * @description Ex: /produtos
+   */
   matcher: string;
+  /**
+   * @title Seções
+   */
   sections: Section[];
 }
 
 export interface ReturnSectionSEO {
+  /**
+   * @title Seções
+   */
   sections: O[];
 }
 
 export interface SectionSEO {
+  /**
+   * @title Seções
+   */
   sections: O[];
 }
 
