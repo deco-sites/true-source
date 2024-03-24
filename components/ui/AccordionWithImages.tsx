70df2a1a2a9294f5730d68cf30ff97e4998617b2
vitoUwu <victorhugomeurerdelgrandi@gmail.com>
2024-03-24 12:34:38 -0400

    feat: sanfoninha component

---
+++ components/ui/AccordionWithImages.tsx
@@ -0,0 +1,284 @@
+import Image from "apps/website/components/Image.tsx";
+import { Picture, Source } from "apps/website/components/Picture.tsx";
+import { AppContext } from "deco-sites/true-source/apps/site.ts";
+import Icon from "deco-sites/true-source/components/ui/Icon.tsx";
+import BannerCarouselJS from "deco-sites/true-source/islands/BannerCarouselJS.tsx";
+import { useId } from "deco-sites/true-source/sdk/useId.ts";
+
+interface DesktopImage {
+  /**
+   * @title Imagem
+   * @format image-uri
+   */
+  src: string;
+  /**
+   * @title Largura
+   * @description É recomendado uma largura de 864px
+   */
+  width: number;
+  /**
+   * @title Altura
+   * @description É recomendado uma altura de 600px
+   */
+  height: number;
+}
+
+interface MobileImage {
+  /**
+   * @title Imagem
+   * @format image-uri
+   */
+  src: string;
+  /**
+   * @title Largura
+   * @description É recomendado uma largura de 390px
+   */
+  width: number;
+  /**
+   * @title Altura
+   * @description É recomendado uma altura de 442px
+   */
+  height: number;
+}
+
+interface ImageProps {
+  /**
+   * @title Desktop
+   * @description Imagem otimizada para telas maiores
+   */
+  desktop: DesktopImage;
+  /**
+   * @title Mobile
+   * @description Imagem otimizada para telas menores
+   */
+  mobile: MobileImage;
+  /**
+   * @title Alt
+   * @description Texto alternativo para a imagem
+   */
+  alt: string;
+}
+
+interface Button {
+  /**
+   * @title Texto
+   */
+  text: string;
+  /**
+   * @title Link
+   */
+  href: string;
+}
+
+/**
+ * @title {{title}} - {{description}}
+ */
+interface Section {
+  /**
+   * @title Título
+   */
+  title: string;
+  /**
+   * @title Descrição
+   */
+  description: string;
+  /**
+   * @title Texto da Tag
+   * @description Texto que será exibido na tag do canto superior esquerdo da imagem
+   */
+  tag: string;
+  /**
+   * @title Botão
+   */
+  button: Button;
+  image: ImageProps;
+}
+
+interface Props {
+  sections: Section[];
+  /**
+   * @title Pré-carregar imagens
+   * @description Ative esta opção quando esse banner é a maior imagem na tela
+   */
+  preload?: boolean;
+}
+
+export function loader(props: Props, _req: Request, ctx: AppContext) {
+  return { ...props, isMobile: ctx.device !== "desktop" };
+}
+
+export default function AccordionWithImages(
+  { sections, preload, isMobile }: ReturnType<typeof loader>,
+) {
+  if (isMobile) {
+    const id = useId();
+
+    return (
+      <div class="mx-auto max-w-[1440px]">
+        <div
+          id={id}
+          class="flex w-full relative"
+        >
+          <ul data-slider class="w-full grid grid-cols-1 grid-rows-1 h-[442px]">
+            {sections.map((section, index) => (
+              <li
+                id={`${id}::${index}`}
+                data-intersecting={index === 0}
+                data-item
+                class="col-start-1 row-start-1 w-full group data-[intersecting='true']:opacity-100 opacity-0 transition-all duration-700 pointer-events-none data-[intersecting='true']:pointer-events-auto"
+              >
+                <div
+                  id={id}
+                  class="relative overflow-clip w-full h-full"
+                >
+                  <span class="bg-gradient-to-t from-black/30 to-transparent inset-0 absolute pointer-events-none" />
+                  <span class="absolute bg-ice font-bold text-[13px] uppercase text-dark leading-[18px] p-3 rounded-full top-8 left-8 font-lemon transition-all">
+                    {section.tag}
+                  </span>
+                  <div class="absolute left-8 bottom-[72px] text-ice">
+                    <h2 class="font-bold text-lg leading-6 font-lemon mb-2">
+                      {section.title}
+                    </h2>
+                    <p class="text-sm leading-4 mb-2">{section.description}</p>
+                    <a
+                      class="bg-gradient-to-r w-fit from-red to-orange font-lemon font-bold text-[13px] leading-[18px] flex items-center justify-center gap-4 transition-all py-3 px-6 rounded-full"
+                      href={section.button.href}
+                    >
+                      {section.button.text}
+                      <Icon id="BannerArrowRight" strokeWidth={2} size={16} />
+                    </a>
+                  </div>
+                  <Picture preload={preload}>
+                    <Source
+                      media="(max-width: 767px)"
+                      fetchPriority={preload ? "high" : "auto"}
+                      src={section.image.mobile.src}
+                      width={section.image.mobile.width}
+                      height={section.image.mobile.height}
+                    />
+                    <Source
+                      media="(min-width: 768px)"
+                      fetchPriority={preload ? "high" : "auto"}
+                      src={section.image.desktop.src}
+                      width={section.image.desktop.width}
+                      height={section.image.desktop.height}
+                    />
+                    <img
+                      class="object-cover w-full h-full"
+                      loading={preload ? "eager" : "lazy"}
+                      src={section.image.desktop.src}
+                      alt={section.image.alt}
+                    />
+                  </Picture>
+                </div>
+              </li>
+            ))}
+          </ul>
+          <div class="absolute left-8 bottom-8 right-8 flex justify-between items-center gap-8 z-[1]">
+            <ul class="carousel justify-center gap-3">
+              {sections.map((_, index) => (
+                <li data-dot={index} class="carousel-item group">
+                  <div class="size-1.5 lg:size-2 rounded-full bg-white group-data-[active]:bg-gradient-to-tr from-red to-orange" />
+                </li>
+              ))}
+            </ul>
+            <div class="flex justify-center items-center gap-2 text-dark">
+              <button
+                data-prev={id}
+                aria-label="Anterior"
+                class="rounded-full bg-white px-2.5 py-1.5 lg:px-4 lg:py-2 flex justify-center items-center"
+              >
+                <Icon
+                  class="rotate-180 size-4 lg:size-6"
+                  id="BannerArrowRight"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                />
+              </button>
+              <button
+                data-next={id}
+                aria-label="Próximo"
+                class="rounded-full bg-white px-2.5 py-1.5 lg:px-4 lg:py-2 flex justify-center items-center"
+              >
+                <Icon
+                  class="size-4 lg:size-6"
+                  id="BannerArrowRight"
+                  strokeWidth={2}
+                  strokeLinecap="round"
+                />
+              </button>
+            </div>
+          </div>
+          <BannerCarouselJS rootId={id} />
+        </div>
+      </div>
+    );
+  }
+
+  return (
+    <ul class="flex px-10 text-ice max-w-[1400px] mx-auto h-[600px]">
+      {sections.map((section, index) => (
+        <li
+          class={"w-full relative transition-all group first:rounded-l-[20px] last:rounded-r-[20px] overflow-clip" +
+            (
+              index === 0
+                ? " has-[~:hover]:max-w-[160px] lg:has-[~:hover]:max-w-[248px] max-w-full"
+                : " max-w-[160px] lg:max-w-[248px] hover:max-w-full"
+            )}
+          key={section.title}
+        >
+          <span
+            class={"absolute bg-ice font-bold text-[13px] uppercase text-dark leading-[18px] p-3 rounded-full top-10 left-10 font-lemon transition-all z-[1]" +
+              (
+                index === 0
+                  ? " group-has-[~:hover]:opacity-0 opacity-100"
+                  : " opacity-0 group-hover:opacity-100"
+              )}
+          >
+            {section.tag}
+          </span>
+          <div
+            class={"absolute bottom-10 z-[1] transition-all" +
+              (
+                index === 0
+                  ? " group-has-[~:hover]:left-[25px] left-10 group-has-[~:hover]:max-w-[185px] max-w-full"
+                  : " left-[25px] max-w-[185px] group-hover:max-w-full group-hover:left-10"
+              )}
+          >
+            <h2 class="font-bold text-lg leading-6 font-lemon mb-2">
+              {section.title}
+            </h2>
+            <p class="text-sm leading-4">{section.description}</p>
+          </div>
+          <a
+            class={"bg-gradient-to-r from-red to-orange font-lemon font-bold text-[13px] leading-[18px] flex items-center justify-center gap-4 absolute right-10 bottom-10 transition-all z-[1] py-3 px-6 rounded-full" +
+              (
+                index === 0
+                  ? " group-has-[~:hover]:opacity-0 opacity-100"
+                  : " opacity-0 group-hover:opacity-100"
+              )}
+            href={section.button.href}
+          >
+            {section.button.text}
+            <Icon id="BannerArrowRight" strokeWidth={2} size={16} />
+          </a>
+          <span
+            class={"bg-gradient-to-t from-black/30 inset-0 absolute pointer-events-none" +
+              (
+                index === 0
+                  ? " group-has-[~:hover]:to-black/30 to-transparent"
+                  : " to-black/30 group-hover:to-transparent"
+              )}
+          />
+          <Image
+            class="w-full h-full object-cover"
+            src={section.image.desktop.src}
+            width={section.image.desktop.width}
+            height={section.image.desktop.height}
+            alt={section.image.alt}
+          />
+        </li>
+      ))}
+    </ul>
+  );
+}
--- manifest.gen.ts
+++ manifest.gen.ts
@@ -30,44 +30,45 @@ import * as $$$$$$19 from "./sections/Content/VideoModal.tsx";
 import * as $$$$$$20 from "./sections/Footer/Footer.tsx";
 import * as $$$$$$21 from "./sections/Gallery.tsx";
 import * as $$$$$$22 from "./sections/Header/Header.tsx";
-import * as $$$$$$23 from "./sections/Images/BannerGrid.tsx";
-import * as $$$$$$24 from "./sections/Images/Carousel.tsx";
-import * as $$$$$$25 from "./sections/Images/ImageGallery.tsx";
-import * as $$$$$$26 from "./sections/Images/ShoppableBanner.tsx";
-import * as $$$$$$27 from "./sections/Layout/Container.tsx";
-import * as $$$$$$28 from "./sections/Layout/Flex.tsx";
-import * as $$$$$$29 from "./sections/Layout/Grid.tsx";
-import * as $$$$$$30 from "./sections/Layout/GridItem.tsx";
-import * as $$$$$$31 from "./sections/Links/LinkTree.tsx";
-import * as $$$$$$32 from "./sections/Links/Shortcuts.tsx";
-import * as $$$$$$33 from "./sections/Miscellaneous/CampaignTimer.tsx";
-import * as $$$$$$34 from "./sections/Miscellaneous/CookieConsent.tsx";
-import * as $$$$$$35 from "./sections/Miscellaneous/FloatingButtons.tsx.tsx";
-import * as $$$$$$36 from "./sections/Miscellaneous/Slide.tsx";
-import * as $$$$$$37 from "./sections/Newsletter/Newsletter.tsx";
-import * as $$$$$$46 from "./sections/Product/Description.tsx";
-import * as $$$$$$47 from "./sections/Product/ImageGalleryFrontBack.tsx";
-import * as $$$$$$48 from "./sections/Product/ImageGallerySlider.tsx";
-import * as $$$$$$49 from "./sections/Product/NotFound.tsx";
-import * as $$$$$$50 from "./sections/Product/NotFoundChallenge.tsx";
-import * as $$$$$$51 from "./sections/Product/ProductInfo.tsx";
-import * as $$$$$$52 from "./sections/Product/ProductMain.tsx";
-import * as $$$$$$53 from "./sections/Product/ProductShelf.tsx";
-import * as $$$$$$54 from "./sections/Product/SearchResult.tsx";
-import * as $$$$$$55 from "./sections/Product/ShelfWithImage.tsx";
-import * as $$$$$$56 from "./sections/Product/SubscriptionModal.tsx";
-import * as $$$$$$57 from "./sections/Product/Wishlist.tsx";
-import * as $$$$$$38 from "./sections/ProductDescription/ProductBenefits.tsx";
-import * as $$$$$$39 from "./sections/ProductDescription/ProductComparison.tsx";
-import * as $$$$$$40 from "./sections/ProductDescription/ProductContainer.tsx";
-import * as $$$$$$41 from "./sections/ProductDescription/ProductGallery.tsx";
-import * as $$$$$$42 from "./sections/ProductDescription/ProductInfoCard.tsx";
-import * as $$$$$$43 from "./sections/ProductDescription/ProductNutritionalHighlights.tsx";
-import * as $$$$$$44 from "./sections/ProductDescription/ProductNutritionalTable.tsx";
-import * as $$$$$$45 from "./sections/ProductDescription/ProductSpace.tsx";
-import * as $$$$$$58 from "./sections/Social/InstagramPosts.tsx";
-import * as $$$$$$59 from "./sections/Social/WhatsApp.tsx";
-import * as $$$$$$60 from "./sections/Theme/Theme.tsx";
+import * as $$$$$$23 from "./sections/Images/AccordionWithImages.tsx";
+import * as $$$$$$24 from "./sections/Images/BannerGrid.tsx";
+import * as $$$$$$25 from "./sections/Images/Carousel.tsx";
+import * as $$$$$$26 from "./sections/Images/ImageGallery.tsx";
+import * as $$$$$$27 from "./sections/Images/ShoppableBanner.tsx";
+import * as $$$$$$28 from "./sections/Layout/Container.tsx";
+import * as $$$$$$29 from "./sections/Layout/Flex.tsx";
+import * as $$$$$$30 from "./sections/Layout/Grid.tsx";
+import * as $$$$$$31 from "./sections/Layout/GridItem.tsx";
+import * as $$$$$$32 from "./sections/Links/LinkTree.tsx";
+import * as $$$$$$33 from "./sections/Links/Shortcuts.tsx";
+import * as $$$$$$34 from "./sections/Miscellaneous/CampaignTimer.tsx";
+import * as $$$$$$35 from "./sections/Miscellaneous/CookieConsent.tsx";
+import * as $$$$$$36 from "./sections/Miscellaneous/FloatingButtons.tsx.tsx";
+import * as $$$$$$37 from "./sections/Miscellaneous/Slide.tsx";
+import * as $$$$$$38 from "./sections/Newsletter/Newsletter.tsx";
+import * as $$$$$$47 from "./sections/Product/Description.tsx";
+import * as $$$$$$48 from "./sections/Product/ImageGalleryFrontBack.tsx";
+import * as $$$$$$49 from "./sections/Product/ImageGallerySlider.tsx";
+import * as $$$$$$50 from "./sections/Product/NotFound.tsx";
+import * as $$$$$$51 from "./sections/Product/NotFoundChallenge.tsx";
+import * as $$$$$$52 from "./sections/Product/ProductInfo.tsx";
+import * as $$$$$$53 from "./sections/Product/ProductMain.tsx";
+import * as $$$$$$54 from "./sections/Product/ProductShelf.tsx";
+import * as $$$$$$55 from "./sections/Product/SearchResult.tsx";
+import * as $$$$$$56 from "./sections/Product/ShelfWithImage.tsx";
+import * as $$$$$$57 from "./sections/Product/SubscriptionModal.tsx";
+import * as $$$$$$58 from "./sections/Product/Wishlist.tsx";
+import * as $$$$$$39 from "./sections/ProductDescription/ProductBenefits.tsx";
+import * as $$$$$$40 from "./sections/ProductDescription/ProductComparison.tsx";
+import * as $$$$$$41 from "./sections/ProductDescription/ProductContainer.tsx";
+import * as $$$$$$42 from "./sections/ProductDescription/ProductGallery.tsx";
+import * as $$$$$$43 from "./sections/ProductDescription/ProductInfoCard.tsx";
+import * as $$$$$$44 from "./sections/ProductDescription/ProductNutritionalHighlights.tsx";
+import * as $$$$$$45 from "./sections/ProductDescription/ProductNutritionalTable.tsx";
+import * as $$$$$$46 from "./sections/ProductDescription/ProductSpace.tsx";
+import * as $$$$$$59 from "./sections/Social/InstagramPosts.tsx";
+import * as $$$$$$60 from "./sections/Social/WhatsApp.tsx";
+import * as $$$$$$61 from "./sections/Theme/Theme.tsx";
 
 const manifest = {
   "loaders": {
@@ -101,54 +102,55 @@ const manifest = {
     "deco-sites/true-source/sections/Footer/Footer.tsx": $$$$$$20,
     "deco-sites/true-source/sections/Gallery.tsx": $$$$$$21,
     "deco-sites/true-source/sections/Header/Header.tsx": $$$$$$22,
-    "deco-sites/true-source/sections/Images/BannerGrid.tsx": $$$$$$23,
-    "deco-sites/true-source/sections/Images/Carousel.tsx": $$$$$$24,
-    "deco-sites/true-source/sections/Images/ImageGallery.tsx": $$$$$$25,
-    "deco-sites/true-source/sections/Images/ShoppableBanner.tsx": $$$$$$26,
-    "deco-sites/true-source/sections/Layout/Container.tsx": $$$$$$27,
-    "deco-sites/true-source/sections/Layout/Flex.tsx": $$$$$$28,
-    "deco-sites/true-source/sections/Layout/Grid.tsx": $$$$$$29,
-    "deco-sites/true-source/sections/Layout/GridItem.tsx": $$$$$$30,
-    "deco-sites/true-source/sections/Links/LinkTree.tsx": $$$$$$31,
-    "deco-sites/true-source/sections/Links/Shortcuts.tsx": $$$$$$32,
-    "deco-sites/true-source/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$33,
-    "deco-sites/true-source/sections/Miscellaneous/CookieConsent.tsx": $$$$$$34,
+    "deco-sites/true-source/sections/Images/AccordionWithImages.tsx": $$$$$$23,
+    "deco-sites/true-source/sections/Images/BannerGrid.tsx": $$$$$$24,
+    "deco-sites/true-source/sections/Images/Carousel.tsx": $$$$$$25,
+    "deco-sites/true-source/sections/Images/ImageGallery.tsx": $$$$$$26,
+    "deco-sites/true-source/sections/Images/ShoppableBanner.tsx": $$$$$$27,
+    "deco-sites/true-source/sections/Layout/Container.tsx": $$$$$$28,
+    "deco-sites/true-source/sections/Layout/Flex.tsx": $$$$$$29,
+    "deco-sites/true-source/sections/Layout/Grid.tsx": $$$$$$30,
+    "deco-sites/true-source/sections/Layout/GridItem.tsx": $$$$$$31,
+    "deco-sites/true-source/sections/Links/LinkTree.tsx": $$$$$$32,
+    "deco-sites/true-source/sections/Links/Shortcuts.tsx": $$$$$$33,
+    "deco-sites/true-source/sections/Miscellaneous/CampaignTimer.tsx": $$$$$$34,
+    "deco-sites/true-source/sections/Miscellaneous/CookieConsent.tsx": $$$$$$35,
     "deco-sites/true-source/sections/Miscellaneous/FloatingButtons.tsx.tsx":
-      $$$$$$35,
-    "deco-sites/true-source/sections/Miscellaneous/Slide.tsx": $$$$$$36,
-    "deco-sites/true-source/sections/Newsletter/Newsletter.tsx": $$$$$$37,
-    "deco-sites/true-source/sections/Product/Description.tsx": $$$$$$46,
+      $$$$$$36,
+    "deco-sites/true-source/sections/Miscellaneous/Slide.tsx": $$$$$$37,
+    "deco-sites/true-source/sections/Newsletter/Newsletter.tsx": $$$$$$38,
+    "deco-sites/true-source/sections/Product/Description.tsx": $$$$$$47,
     "deco-sites/true-source/sections/Product/ImageGalleryFrontBack.tsx":
-      $$$$$$47,
-    "deco-sites/true-source/sections/Product/ImageGallerySlider.tsx": $$$$$$48,
-    "deco-sites/true-source/sections/Product/NotFound.tsx": $$$$$$49,
-    "deco-sites/true-source/sections/Product/NotFoundChallenge.tsx": $$$$$$50,
-    "deco-sites/true-source/sections/Product/ProductInfo.tsx": $$$$$$51,
-    "deco-sites/true-source/sections/Product/ProductMain.tsx": $$$$$$52,
-    "deco-sites/true-source/sections/Product/ProductShelf.tsx": $$$$$$53,
-    "deco-sites/true-source/sections/Product/SearchResult.tsx": $$$$$$54,
-    "deco-sites/true-source/sections/Product/ShelfWithImage.tsx": $$$$$$55,
-    "deco-sites/true-source/sections/Product/SubscriptionModal.tsx": $$$$$$56,
-    "deco-sites/true-source/sections/Product/Wishlist.tsx": $$$$$$57,
+      $$$$$$48,
+    "deco-sites/true-source/sections/Product/ImageGallerySlider.tsx": $$$$$$49,
+    "deco-sites/true-source/sections/Product/NotFound.tsx": $$$$$$50,
+    "deco-sites/true-source/sections/Product/NotFoundChallenge.tsx": $$$$$$51,
+    "deco-sites/true-source/sections/Product/ProductInfo.tsx": $$$$$$52,
+    "deco-sites/true-source/sections/Product/ProductMain.tsx": $$$$$$53,
+    "deco-sites/true-source/sections/Product/ProductShelf.tsx": $$$$$$54,
+    "deco-sites/true-source/sections/Product/SearchResult.tsx": $$$$$$55,
+    "deco-sites/true-source/sections/Product/ShelfWithImage.tsx": $$$$$$56,
+    "deco-sites/true-source/sections/Product/SubscriptionModal.tsx": $$$$$$57,
+    "deco-sites/true-source/sections/Product/Wishlist.tsx": $$$$$$58,
     "deco-sites/true-source/sections/ProductDescription/ProductBenefits.tsx":
-      $$$$$$38,
-    "deco-sites/true-source/sections/ProductDescription/ProductComparison.tsx":
       $$$$$$39,
-    "deco-sites/true-source/sections/ProductDescription/ProductContainer.tsx":
+    "deco-sites/true-source/sections/ProductDescription/ProductComparison.tsx":
       $$$$$$40,
-    "deco-sites/true-source/sections/ProductDescription/ProductGallery.tsx":
+    "deco-sites/true-source/sections/ProductDescription/ProductContainer.tsx":
       $$$$$$41,
-    "deco-sites/true-source/sections/ProductDescription/ProductInfoCard.tsx":
+    "deco-sites/true-source/sections/ProductDescription/ProductGallery.tsx":
       $$$$$$42,
-    "deco-sites/true-source/sections/ProductDescription/ProductNutritionalHighlights.tsx":
+    "deco-sites/true-source/sections/ProductDescription/ProductInfoCard.tsx":
       $$$$$$43,
-    "deco-sites/true-source/sections/ProductDescription/ProductNutritionalTable.tsx":
+    "deco-sites/true-source/sections/ProductDescription/ProductNutritionalHighlights.tsx":
       $$$$$$44,
-    "deco-sites/true-source/sections/ProductDescription/ProductSpace.tsx":
+    "deco-sites/true-source/sections/ProductDescription/ProductNutritionalTable.tsx":
       $$$$$$45,
-    "deco-sites/true-source/sections/Social/InstagramPosts.tsx": $$$$$$58,
-    "deco-sites/true-source/sections/Social/WhatsApp.tsx": $$$$$$59,
-    "deco-sites/true-source/sections/Theme/Theme.tsx": $$$$$$60,
+    "deco-sites/true-source/sections/ProductDescription/ProductSpace.tsx":
+      $$$$$$46,
+    "deco-sites/true-source/sections/Social/InstagramPosts.tsx": $$$$$$59,
+    "deco-sites/true-source/sections/Social/WhatsApp.tsx": $$$$$$60,
+    "deco-sites/true-source/sections/Theme/Theme.tsx": $$$$$$61,
   },
   "apps": {
     "deco-sites/true-source/apps/decohub.ts": $$$$$$$$$$$0,
