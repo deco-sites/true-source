444049d4f152d5253f93d216eddc36b9298480a4
lui-dias <66072698+lui-dias@users.noreply.github.com>
2024-03-26 18:43:07 -0300

    fix: My account iframe height

--- sections/Miscellaneous/ProxyIframe.tsx
+++ sections/Miscellaneous/ProxyIframe.tsx
@@ -1,28 +1,28 @@
+
 // deno-lint-ignore-file
 interface Props {
     src?: string;
   }
   
   const runOnMount = () => {
-    window.onload = () => {
+    globalThis.onload = () => {
       const iFrame = document.getElementById(
         "proxy-loader",
       ) as HTMLIFrameElement;
       if (!iFrame) {
         return console.error("Couldn't find iframe");
       }
-      iFrame.height = `${iFrame.contentWindow?.document.body.scrollHeight}`;
+      iFrame.height = `${iFrame.contentWindow?.document.querySelector('.vtex-account')?.scrollHeight}`;
     };
   };
   
   export default function ProxyIframe({ src }: Props) {
     return (
       <>
-        <script dangerouslySetInnerHTML={{ __html: `(${runOnMount})();` }}>
-        </script>
+        <script dangerouslySetInnerHTML={{ __html: `(${runOnMount})();` }} />
         <iframe
           id="proxy-loader"
-          style="width:100%;border:none;overflow:hidden;min-height:950px;"
+          style="width:100%;border:none;overflow:hidden"
           src={src}
           // onload='javascript:(function(o){o.style.height=o.contentWindow.document.body.scrollHeight+"px";}(this));'
         >
@@ -30,3 +30,4 @@ interface Props {
       </>
     );
   }
+  
